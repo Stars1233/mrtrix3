Stars1233/mrtrix3

commit ec2c2da7b1dd161004d1f395374cc75f44ff3044

Improve check_keyvals() to show all differences
Rather than failing only at the first difference encountered.

core/image_diff.h
@@ -112,33 +112,45 @@ namespace MR
   template <class HeaderType1, class HeaderType2>
     inline void check_keyvals (const HeaderType1& in1, const HeaderType2& in2)
     {
-      std::map<std::string, std::string>::const_iterator it1 = in1.keyval().begin(), it2 = in2.keyval().begin();
       const static std::set<std::string> reserved { "command_history", "mrtrix_version", "project_version" };
-      while (it1 != in1.keyval().end()) {
-        if (reserved.find (it1->first) != reserved.end()) {
+      auto it1 = in1.keyval().cbegin();
+      auto it2 = in2.keyval().cbegin();
+      Exception errors;
+      while (it1 != in1.keyval().end() || it2 != in2.keyval().end()) {
+        while (it1 != in1.keyval().end() && reserved.find (it1->first) != reserved.end())
           ++it1;
-          continue;
-        }
-        if (it2 != in2.keyval().end() && reserved.find (it2->first) != reserved.end()) {
+        while (it2 != in2.keyval().end() && reserved.find (it2->first) != reserved.end())
           ++it2;
-          continue;
+
+        if (it1 == in1.keyval().end() || it2 == in2.keyval().end())
+          break;
+
+        if (it1 == in1.keyval().end()) {
+          errors.push_back ("Key \"" + it2->first + "\" in image \"" + in2.name() + "\" not present in \"" + in1.name() + "\"");
+          ++it2;
         }
-        if (it2 == in2.keyval().end())
-          throw Exception ("Key \"" + it1->first + "\" in image \"" + in1.name() + "\" not present in \"" + in2.name() + "\"");
-        if (it1->first != it2->first) {
-          if (in2.keyval().find (it1->first) == in2.keyval().end())
-            throw Exception ("Key \"" + it1->first + "\" in image \"" + in1.name() + "\" not present in \"" + in2.name() + "\"");
-          else
-            throw Exception ("Key \"" + it2->first + "\" in image \"" + in2.name() + "\" not present in \"" + in1.name() + "\"");
+        else if (it2 == in2.keyval().end()) {
+          errors.push_back ("Key \"" + it1->first + "\" in image \"" + in1.name() + "\" not present in \"" + in2.name() + "\"");
+          ++it1;
         }
-        if (it1->second != it2->second)
-          throw Exception ("Key \"" + it1->first + "\" has different values between images");
-        ++it1; ++it2;
+        else if (it1->first < it2->first) {
+          errors.push_back ("Key \"" + it1->first + "\" in image \"" + in1.name() + "\" not present in \"" + in2.name() + "\"");
+          ++it1;
+        }
+        else if (it1->first > it2->first) {
+          errors.push_back ("Key \"" + it2->first + "\" in image \"" + in2.name() + "\" not present in \"" + in1.name() + "\"");
+          ++it2;
+        }
+        else {
+          if (it1->second != it2->second)
+            errors.push_back ("Key \"" + it1->first + "\" has different values between images");
+          ++it1;
+          ++it2;
+        }
       }
-      while (it2 != in2.keyval().end() && reserved.find (it2->first) != reserved.end())
-        ++it2;
-      if (it2 != in2.keyval().end())
-        throw Exception ("Key \"" + it2->first + "\" in image \"" + in2.name() + "\" not present in \"" + in1.name() + "\"");
+
+      if (errors.num() > 0)
+        throw errors;
     }
 
     //! check image headers are the same (dimensions, spacing & transform)

core/exception.h
@@ -79,6 +79,8 @@ namespace MR
 
   class Exception { NOMEMALIGN
     public:
+      Exception () { }
+
       Exception (const std::string& msg) {
         description.push_back (msg);
       }

commit 47772bcac179dd4c88ea7a157cf08acddc56ee32

MR::check_keyvals(): Fix checks at ends of dicts
Error introduced in ec2c2da7b1dd161004d1f395374cc75f44ff3044 as part of #2346.

core/image_diff.h
@@ -122,7 +122,7 @@ namespace MR
         while (it2 != in2.keyval().end() && reserved.find (it2->first) != reserved.end())
           ++it2;
 
-        if (it1 == in1.keyval().end() || it2 == in2.keyval().end())
+        if (it1 == in1.keyval().end() && it2 == in2.keyval().end())
           break;
 
         if (it1 == in1.keyval().end()) {